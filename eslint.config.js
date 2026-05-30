562843e221af80d74b7b3ca08b17fea3441f0d71
Romina Montiel <romiiemma@hotmail.com>
2026-05-30 18:36:09 -0300

    Estructura principal de componentes

--- eslint.config.js
+++ eslint.config.js
@@ -29,7 +29,7 @@ module.exports = defineConfig([
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: 'rm',
           style: 'kebab-case',
         },
       ],
---
+++ src/app/features/landing/pages/landing-page/landing-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rm-landing-page',
+  imports: [],
+  templateUrl: './landing-page.html',
+  styleUrl: './landing-page.scss',
+})
+export default class LandingPage {}
