e9556f6279a5546373c2cfe7b8e670b337f08b72
Romina Montiel <romiiemma@hotmail.com>
2026-06-01 21:35:44 -0300

    Desarrollo seccion about

---
+++ src/app/features/landing/components/about-section/about-section.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { HiearchyUi } from '@core/constants/hiearchy-ui.constant';
+import { SectionTitles } from '@core/constants/sections.constants';
+import { SectionTitle } from '@shared/ui/section-title/section-title';
+import { Tag } from '@shared/ui/tag/tag';
+
+@Component({
+  selector: 'rm-about-section',
+  imports: [SectionTitle, Tag],
+  templateUrl: './about-section.html',
+  styleUrl: './about-section.scss',
+})
+export class AboutSection {
+  public titleSection = SectionTitles;
+  public tagList = [
+    'Typescript',
+    'RxJs',
+    'Signals',
+    'Standalone Components',
+    'Unit tests',
+    'Karma/Jasmine',
+    'Vitest',
+    'Lazy Loading',
+    'GIT',
+    'Figma',
+    'Atomic Design',
+    'Autolayout',
+  ];
+  public tagsHiearchy = HiearchyUi;
+}
